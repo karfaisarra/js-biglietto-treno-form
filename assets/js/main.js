7ed73a76a572bdb5720a4fd91434d85fad1d32dd
karfaisarra <karfaisarra@gmail.com>
2022-10-07 17:55:45 +0200

    add commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,22 +18,25 @@ element.addEventListener('click', function(){
     console.log(age.value);
     //il prezzo del biglietto è definito in base ai km (0.21 € al km).
     const pricePerKm = 0.21;
-    const initialPrice = kilometersToGo * pricePerKm;
+    const initialPrice = kilometersToGo.value * pricePerKm;
     console.log(initialPrice);
     //va applicato uno sconto del 20% per i minorenni e 40% per i over 65.
     if (age.value < 18 ) {
         const sconto = 20 / 100;
         price = initialPrice - (initialPrice * sconto);
         console.log('sono minorenne');
+        console.log('price', price);
         discount = 'Sconto Minorenne';
     } else if (age.value > 65) {
         const sconto = 40 / 100;
         price = initialPrice - (initialPrice * sconto);
         console.log('sono vecchio');
+        console.log('price', price);
         discount = 'Sconto di 40%';
     } else {
         price = initialPrice;
         console.log('sono maggiorenne' );
+        console.log('price', price);
         discount = 'Nessun Sconto da applicare';
     }
     document.getElementById('name').innerHTML = `${userName.value}`;
